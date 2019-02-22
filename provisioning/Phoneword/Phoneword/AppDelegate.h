84cf4d3b3b3282ed33ca2d16a24a51a19c900575
Greg Orton <gorton@pillartechnology.com>
2019-02-22 15:44:22 -0600

    everything

---
+++ provisioning/Phoneword/Phoneword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Phoneword
+//
+//  Created by Greg Orton on 1/15/19.
+//  Copyright © 2019 Greg Orton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ provisioning/Phoneword/Phoneword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phoneword
+//
+//  Created by Greg Orton on 1/15/19.
+//  Copyright © 2019 Greg Orton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
